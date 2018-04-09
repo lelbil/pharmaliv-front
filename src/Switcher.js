5f223eea22d73678ff534db81669e8f69447e3e3
Billel Attouchi <billel.attouchi@ekino.com>
2018-04-09 11:36:45 +0200

    delete Yarn.lock

--- src/Switcher.js
+++ src/Switcher.js
@@ -5,12 +5,21 @@ import RegisterView from './RegisterView'
 import App from './App'
 
 class Switcher extends Component {
+
+    componentWillMount() {
+        //TODO: call API to receive user type if logged in
+        this.setState({
+            loggedIn: false
+        })
+    }
+
     render() {
         return (
             <Switch>
                 <Route path="/login" component={LoginView} />
                 <Route path="/app" component={App} />
                 <Route path="/register" component={RegisterView} />
+                <Route exactPath="/" component={this.state.loggedIn? App : LoginView}/>
             </Switch>
         )
     }
