cbd77e6119eb22e441e61823e27c950a5e742395
Billel Attouchi <billel.attouchi@ekino.com>
2018-06-05 12:22:38 +0200

    show cart button for patients only

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
 import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
@@ -10,8 +10,8 @@ import './Header.css'
 import history from './JS/history'
 import RegisterView from './RegisterView'
 
-import { API_URL } from './JS/constants'
-import { getExactInfo } from './JS/utils'
+import {API_URL} from './JS/constants'
+import {getExactInfo} from './JS/utils'
 
 const profileText = "Modifier mes infos"
 const logout = "Déconnexion"
@@ -28,7 +28,7 @@ class Header extends Component {
     }
 
     disconnect() {
-        fetch(`${API_URL}/logout`, { credentials: 'include' })
+        fetch(`${API_URL}/logout`, {credentials: 'include'})
             .then(response => {
                 if (response.status !== 200) console.log('Received an unexpected response from API when trying to login')
                 history.push('/') //This will redirect the user to the home page even if the logging out failed. So it's offering a degraded service but at least no errors
@@ -39,20 +39,20 @@ class Header extends Component {
     }
 
     openPanier = () => {
-        fetch(`${API_URL}/cart`, { credentials: 'include'})
+        fetch(`${API_URL}/cart`, {credentials: 'include'})
             .then(response => response.json())
             .then(panier => {
-                this.setState({ panier, panierOpen: true })
+                this.setState({panier, panierOpen: true})
             })
             .catch(error => {
                 //Open panier anyway, it will show previously fetched products or nothing, but at least it will respond
-                this.setState({ panierOpen: true })
+                this.setState({panierOpen: true})
                 console.log('ERROR GETTING CART INFO', error)
             })
     }
 
     openModifyMyInfo = () => {
-        fetch(`${API_URL}/info`, { credentials: 'include'})
+        fetch(`${API_URL}/info`, {credentials: 'include'})
             .then(response => response.json())
             .then(info => {
                 const myInfo = getExactInfo(info)
@@ -70,7 +70,7 @@ class Header extends Component {
     }
 
     closeDialog = () => {
-        this.setState({ panierOpen: false, modifyMyInfo: false })
+        this.setState({panierOpen: false, modifyMyInfo: false})
     }
 
     confirmCart = () => {//TODO: should have some sort of redirection to a payment page
@@ -85,29 +85,29 @@ class Header extends Component {
                 'Content-Type': 'application/json',
             },
         })
-        .then(response => {
-            if (response.status === 200 ) {
-                this.setState({
-                    panier: [],
-                    panierOpen: false,
-                })
-                //TODO: Why not add a snackbar to say that it was ordered just fine?
-            }
-            else {
-                console.log('GOT UNEXPECTED RESPONSE STATUS CODE WHEN TRYING TO ORDER')
-            }
-        })
-        .catch(error => {
-            console.log("ERROR", error)
-        })
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({
+                        panier: [],
+                        panierOpen: false,
+                    })
+                    //TODO: Why not add a snackbar to say that it was ordered just fine?
+                }
+                else {
+                    console.log('GOT UNEXPECTED RESPONSE STATUS CODE WHEN TRYING TO ORDER')
+                }
+            })
+            .catch(error => {
+                console.log("ERROR", error)
+            })
     }
 
     deleteItemFromCart = productId => {
-        fetch(`${API_URL}/cart/${productId}`, { method: 'DELETE', credentials: 'include'})
+        fetch(`${API_URL}/cart/${productId}`, {method: 'DELETE', credentials: 'include'})
             .then(response => response.json())
             .then(result => {
                 if (result === 1) {
-                    this.setState({ panier: this.state.panier.filter(el => el.panier_id !== productId) })
+                    this.setState({panier: this.state.panier.filter(el => el.panier_id !== productId)})
                 } else {
                     console.log('There has been a problem while deleting element from cart, no rows affected')
                 }
@@ -150,14 +150,18 @@ class Header extends Component {
                             iconButtonElement={<ArrowDropDownCircle/>}
                         >
                             <MenuItem onClick={this.openModifyMyInfo} primaryText={profileText}/>
-                            <MenuItem onClick={this.openPanier} primaryText={cart}/>
+                            {session.type === "patientContent" &&
+                            <MenuItem onClick={this.openPanier} primaryText={cart}/>}
                             <MenuItem onClick={this.disconnect} primaryText={logout}/>
                         </IconMenu>
                     </div>
                     <div id="actions" className="actions">
-                        <RaisedButton className="profileButton" primary={true} label={profileText} onClick={this.openModifyMyInfo}/>
-                        <RaisedButton className="cart" primary={true} label={cart} onClick={this.openPanier}/>
-                        <RaisedButton className="notificationsButton" secondary={true} onClick={this.disconnect} label={logout}/>
+                        <RaisedButton className="profileButton" primary={true} label={profileText}
+                                      onClick={this.openModifyMyInfo}/>
+                        {session.type === "patientContent" &&
+                        <RaisedButton className="cart" primary={true} label={cart} onClick={this.openPanier}/>}
+                        <RaisedButton className="notificationsButton" secondary={true} onClick={this.disconnect}
+                                      label={logout}/>
                     </div>
                 </div>
                 <Dialog
@@ -169,23 +173,29 @@ class Header extends Component {
                     {
                         this.state.panier.map(product => <div>
                             <h2>
-                                <RemoveCircle style={{ color: '#cc0000', cursor: 'pointer', marginRight: '8px'}} onClick={() => {this.deleteItemFromCart(product.panier_id)}}/>
+                                <RemoveCircle style={{color: '#cc0000', cursor: 'pointer', marginRight: '8px'}}
+                                              onClick={() => {
+                                                  this.deleteItemFromCart(product.panier_id)
+                                              }}/>
                                 <span>{product.nom}</span>
-                                <span style={{ float: "right", fontSize: '14px' }}>{parseFloat(product.prix).toFixed(2)}€ x {product.quantite} =
+                                <span style={{float: "right", fontSize: '14px'}}>{parseFloat(product.prix).toFixed(2)}€ x {product.quantite}
+                                    =
                                     <b style={{fontSize: '25px'}}>{parseFloat(product.prix * product.quantite).toFixed(2)}€</b>
                                 </span>
                             </h2>
                             <Divider/>
                         </div>)
                     }
-                    <h1 style={{ float: 'right', marginTop: '30px', marginBottom: '0px' }}>TOTAL: {parseFloat(this.state.panier.reduce((acc, { prix, quantite }) => acc + prix * quantite, 0)).toFixed(2)}€</h1>
+                    <h1 style={{float: 'right', marginTop: '30px', marginBottom: '0px'}}>
+                        TOTAL: {parseFloat(this.state.panier.reduce((acc, {prix, quantite}) => acc + prix * quantite, 0)).toFixed(2)}€</h1>
                 </Dialog>
                 <Dialog
                     title='Modifier Mes Infos'
                     open={this.state.modifyMyInfo}
                     onRequestClose={this.closeDialog}
                 >
-                    <RegisterView isUpdate={true} type={session.type} info={this.state.myInfo} userId={this.state.userId} userInfoId={this.state.userInfoId} />
+                    <RegisterView isUpdate={true} type={session.type} info={this.state.myInfo}
+                                  userId={this.state.userId} userInfoId={this.state.userInfoId}/>
                 </Dialog>
             </div>
         )
