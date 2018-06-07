c25ccdb664af5cf9384333077faee174d91357d3
Billel Attouchi <billel.attouchi@ekino.com>
2018-06-07 10:26:43 +0200

    refactor Switcher

--- src/Switcher.js
+++ src/Switcher.js
@@ -6,43 +6,49 @@ import App from './App'
 
 import { API_URL } from './JS/constants'
 
-let type = false
+export default class Switcher extends Component {
+    constructor(props) {
+        super(props)
 
-class Switcher extends Component {
-
+        this.state = {
+            isLogged: false,
+            sessionInfo: null
+        }
+    }
     componentDidMount() {
-        fetch(`${API_URL}/session`, {credentials: 'include'}).then(response => response.json())
+        fetch(`${API_URL}/session`, {
+            credentials: 'include'
+        })
+            .then(response => response.json())
             .then(data => {
-                type = data.type
-                this.sessionInfo = data
-
-                this.forceUpdate()
+                this.setState({
+                    type: data.type,
+                    sessionInfo: data
+                })
             })
             .catch(error => {
-                console.log('ERROR', error)
+                console.error(error)
             })
     }
 
     render() {
-        const AppComponent = props => {
-            return (
-                <App
-                    type={type}
-                    sessionInfo={this.sessionInfo}
-                    {...props}
-                />
-            );
-        }
+        const { type, sessionInfo } = this.state
+
+        const AppComponent = props => (
+            <App
+                type={type}
+                sessionInfo={sessionInfo}
+                {...props}
+            />
+        )
 
         return (
             <Switch>
                 <Route path="/login" component={LoginView} />
-                <Route path="/app" component={App} />
+                <Route path="/app" render={App} />
                 <Route path="/register" component={RegisterView} />
                 <Route exactPath="/" component={!!type ? AppComponent : LoginView}/>
             </Switch>
         )
     }
 }
-
-export default Switcher
