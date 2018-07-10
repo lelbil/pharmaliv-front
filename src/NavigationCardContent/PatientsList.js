7c9005aeac7359a02614af32ef632421b81e4084
ci <ci@hodor>
2018-07-10 17:44:47 +0200

    load real patient list added by doctor

--- src/NavigationCardContent/PatientsList.js
+++ src/NavigationCardContent/PatientsList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import moment from 'moment'
-import { tableBGColour } from '../JS/constants'
+import {API_URL, tableBGColour} from '../JS/constants'
 
 class OrdersList extends Component {
     constructor(props) {
@@ -12,78 +12,21 @@ class OrdersList extends Component {
         }
     }
 
-    getPatients () {
-        return new Promise((resolve, reject) => {
-            setTimeout(resolve, 100, [
-                {
-                    sex: 'M',
-                    nom: 'Jon Snow',
-                    birthDate: Date.parse('05-05-1987')/1000,
-                    nss: '1 87 08 92 002 039 1',
-                },
-                {
-                    sex: 'F',
-                    nom: 'Rose Leslie',
-                    birthDate: Date.parse('03-06-1997')/1000,
-                    nss: '1 97 08 92 002 039 2',
-                },
-                {
-                    sex: 'M',
-                    nom: 'Jorah Mormont',
-                    birthDate: Date.parse('12-21-1963')/1000,
-                    nss: '1 63 08 92 002 039 1',
-                },
-                {
-                    sex: 'M',
-                    nom: 'Jon Snow',
-                    birthDate: Date.parse('05-05-1987')/1000,
-                    nss: '1 87 08 92 002 039 1',
-                },
-                {
-                    sex: 'M',
-                    nom: 'Jon Snow',
-                    birthDate: Date.parse('05-05-1987')/1000,
-                    nss: '1 87 08 92 002 039 1',
-                },
-                {
-                    sex: 'M',
-                    nom: 'Jon Snow',
-                    birthDate: Date.parse('05-05-1987')/1000,
-                    nss: '1 87 08 92 002 039 1',
-                },
-                {
-                    sex: 'M',
-                    nom: 'Jon Snow',
-                    birthDate: Date.parse('05-05-1987')/1000,
-                    nss: '1 87 08 92 002 039 1',
-                },
-                {
-                    sex: 'M',
-                    nom: 'Jon Snow',
-                    birthDate: Date.parse('05-05-1987')/1000,
-                    nss: '1 87 08 92 002 039 1',
-                },
-                {
-                    sex: 'M',
-                    nom: 'Jon Snow',
-                    birthDate: Date.parse('05-05-1987')/1000,
-                    nss: '1 87 08 92 002 039 1',
-                },
-            ])
-        })
-    }
-
     componentDidMount() {
-        this.getPatients().then(
-            data => this.setState({ data })
-        )
+        fetch(`${API_URL}/mesPatients`, {credentials: 'include'}).then(response => response.json())
+            .then(data => {
+                this.setState({ data: data.map(patient => {return {...patient, dob: Date.parse(patient.dob)/1000}}) })
+            })
+            .catch(error => {
+                console.log('ERROR', error)
+            })
     }
 
     render() {
         const columns = [
             {
-                Header: 'Sexe',
-                accessor: 'sex',
+                Header: 'Prénom',
+                accessor: 'prenom',
             },
             {
                 Header: 'Nom',
@@ -91,12 +34,20 @@ class OrdersList extends Component {
             },
             {
                 Header: 'Date De Naissance',
-                accessor: 'birthDate',
+                accessor: 'dob',
                 Cell: props => <span>{moment.unix(props.value).format('DD-MM-YYYY')}</span>
             },
             {
                 Header: 'Numéro de Sécurité Sociale',
                 accessor: 'nss',
+            },
+            {
+                Header: 'Émail',
+                accessor: 'email',
+            },
+            {
+                Header: 'Numéro de téléphone',
+                accessor: 'tel',
             },
         ]
 
